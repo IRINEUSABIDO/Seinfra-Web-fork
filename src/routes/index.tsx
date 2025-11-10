32e8191942987b724e298463574dd8506ae350be
PedroHenriquee777 <phznff13@gmail.com>
2025-11-10 11:25:08 -0300

    initial setup

---
+++ src/routes/index.tsx
@@ -0,0 +1,6 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { HomePage } from "../view/pages/Home/HomePage";
+
+export const Route = createFileRoute("/")({
+	component: HomePage,
+});
